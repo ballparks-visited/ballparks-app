3472fe67b1e45654d2d959cde7dd3cb7d40723b2
Jay Gossett <jay.d.gossett+github@gmail.com>
2017-03-19 18:49:23 -0500

    Updated Visited Parks to pull info from database and created MapService.js
    Added commands to pull park data (name, location, league, home team)
    from the database.
    
    Created MapService.js to pull location data from database and display it
    on a Google map. This isn't working yet, so it might get replaced if it
    can't be fixed.

---
+++ public/js/services/MapService.js
@@ -0,0 +1,125 @@
+// Creates the MapService factory. This will be the primary means by which we interact with Google Maps
+app.factory('MapService', function($http){
+
+        // Initialize Variables
+        // -------------------------------------------------------------
+        // Service our factory will return
+        var googleMapService = {};
+
+        // Array of locations obtained from API calls
+        var locations = [];
+
+        // Selected Location (initialize to center of America)
+        var selectedLat = 39.50;
+        var selectedLong = -98.35;
+
+        // Functions
+        // --------------------------------------------------------------
+        // Refresh the Map with new data. Function will take new latitude and longitude coordinates.
+        googleMapService.refresh = function(latitude, longitude){
+
+            // Clears the holding array of locations
+            locations = [];
+
+            // Set the selected lat and long equal to the ones provided on the refresh() call
+            selectedLat = latitude;
+            selectedLong = longitude;
+
+            // Perform an AJAX call to get all of the records in the db.
+            $http.get('/api/v1/ballparks').success(function(response){
+
+                // Convert the results into Google Map Format
+                locations = convertToMapPoints(response);
+
+                // Then initialize the map.
+                initialize(latitude, longitude);
+            }).error(function(){});
+        };
+
+        // Private Inner Functions
+        // --------------------------------------------------------------
+        // Convert a JSON of users into map points
+        var convertToMapPoints = function(response){
+
+            // Clear the locations holder
+            var locations = [];
+
+            // Loop through all of the JSON entries provided in the response
+            for(var i= 0; i < response.length; i++) {
+                var ballpark = response[i];
+
+                // Create popup windows for each record
+                var  contentString =
+                    '<p><b>Park Name</b>: ' + ballpark.primary_name +
+                    '<br><b>Home Team</b>: ' + ballpark.home_team +
+                    '<br><b>League</b>: ' + ballpark.league +
+                    '</p>';
+
+                // Converts each of the JSON records into Google Maps Location format (Note [Lat, Lng] format).
+                locations.push({
+                    latlon: new google.maps.LatLng(ballpark.loc[1], ballpark.loc[0]),
+                    message: new google.maps.InfoWindow({
+                        content: contentString,
+                        maxWidth: 320
+                    }),
+                    parkname: ballpark.primary_name,
+                    hometeam: ballpark.home_team,
+                    league: ballpark.league
+            });
+        }
+        // location is now an array populated with records in Google Maps format
+        return locations;
+    };
+
+// Initializes the map
+var initialize = function(latitude, longitude) {
+
+    // Uses the selected lat, long as starting point
+    var myLatLng = {lat: selectedLat, lng: selectedLong};
+
+    // If map has not been created already...
+    if (!map){
+
+        // Create a new map and place in the index.html page
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 3,
+            center: myLatLng
+        });
+    }
+
+    // Loop through each location in the array and place a marker
+    locations.forEach(function(n, i){
+        var marker = new google.maps.Marker({
+            position: n.latlon,
+            map: map,
+            title: "Ballparks Map",
+            icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+        });
+
+        // For each marker created, add a listener that checks for clicks
+        google.maps.event.addListener(marker, 'click', function(e){
+
+            // When clicked, open the selected marker's message
+            currentSelectedMarker = n;
+            n.message.open(map, marker);
+        });
+    });
+
+    // Set initial location as a bouncing red marker
+    var initialLocation = new google.maps.LatLng(latitude, longitude);
+    var marker = new google.maps.Marker({
+        position: initialLocation,
+        animation: google.maps.Animation.BOUNCE,
+        map: map,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+    });
+    lastMarker = marker;
+
+};
+
+// Refresh the page upon window load. Use the initial latitude and longitude
+google.maps.event.addDomListener(window, 'load',
+    googleMapService.refresh(selectedLat, selectedLong));
+
+return googleMapService;
+});
