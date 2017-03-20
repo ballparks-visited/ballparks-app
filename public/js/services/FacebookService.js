b55d486425d99cc5fa5b255bef432f23bf3c93f7
Luke Adair <j.luke.adair@gmail.com>
2017-03-20 12:12:42 -0500

    refactored to get friend photos when getting all friends

--- public/js/services/FacebookService.js
+++ public/js/services/FacebookService.js
@@ -67,18 +67,34 @@ app.factory("FacebookService",function($q, Facebook, AuthService) {
 		return deferred.promise;
 	};
 	
+	// load user friends and their photos
 	service.getFriends = function() {
 		var deferred = $q.defer();
 		var userID = AuthService.getUserId();
+		var userToken = AuthService.getUserToken();
 
-		console.log(userID);
-
-		Facebook.api('/' + userID + '/friends', function(response) {
+		Facebook.api('/' + userID + '/friends', {access_token: userToken, fields: 'first_name, name, id'}, function(response) {
 			if(!response || response.error) {
+				console.log(response.error);
 				deferred.reject('Error occured');
 			}
 			else {
-				deferred.resolve(response);
+				var promises = [];
+				for (var i = 0; i < response.data.length; i++) {
+					var fData = response.data[i];
+					(function(data){
+						var thisPromise = service.getUserPhoto(fData.id)
+						.then(function(userPhoto) {
+							data.photoURL = userPhoto.data.url;
+						});
+						promises.push(thisPromise);
+					})(fData);
+				}
+
+				$q.all(promises)
+				.then(function() {
+					deferred.resolve(response);
+				});
 			}
 		});
 
