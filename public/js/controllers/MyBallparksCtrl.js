5ba9092d9a9fb70db704e343c24d7f5e73c2f963
Luke Adair <j.luke.adair@gmail.com>
2017-03-20 12:14:17 -0500

    finished friend output; finished add ballpark

--- public/js/controllers/MyBallparksCtrl.js
+++ public/js/controllers/MyBallparksCtrl.js
@@ -1,4 +1,4 @@
-app.controller('MyBallparksController', function($scope, $window, UserService, BallparkService, AuthService) {
+app.controller('MyBallparksController', function($scope, $window, UserService, BallparkService, AuthService, FacebookService) {
 	// Authenticate the user
 	if(!AuthService.isTokenValid()) {
 		// redirect to login
@@ -10,11 +10,11 @@ app.controller('MyBallparksController', function($scope, $window, UserService, B
 
 	// load the user
 	function loadUser() {
-		UserService.getUserById(AuthService.getUserId())
+		return UserService.getUserById(AuthService.getUserId())
 		.then(function(response) {
-			// console.log(response);
-			$scope.ballparks = response.data.ballparks;
-			// $scope.$apply();
+			$scope.userBallparks = response.data.ballparks;
+			$scope.user = response.data;
+			calculateBallparkMatches();
 		});
 	};
 	loadUser();
@@ -24,37 +24,106 @@ app.controller('MyBallparksController', function($scope, $window, UserService, B
 	BallparkService.loadBallparks()
 	.then(function(response) {
 		ballparks = response.data;
+		$scope.ballparks = response.data;
 	});
 
-	$scope.showSearch = function() {
-		if(typeof ballparks !== 'undefined') {
-			$scope.searchMode = true;
+	// load user friends
+	FacebookService.getFriends()
+	.then(function(response) {
+		$scope.friends = response.data;
+
+		return response.data.map(function(el) {
+			return el.id
+		}).join(',');
+	})
+	.then(function(friendIds) {
+		return UserService.getFriendData(friendIds);
+	})
+	.then(function(friendBallparkData) {
+		// update friend data with ballparks
+		for (var i = 0; i < friendBallparkData.data.length; i++) {
+			var ballparkData = friendBallparkData.data[i];
+			for (var j = 0; j < $scope.friends.length; j++) {
+				if($scope.friends[j].id === ballparkData.fb_id) {
+					$scope.friends[j].numBallparks = ballparkData.ballparks.length;
+					$scope.friends[j].ballparks = ballparkData.ballparks.map(function(el) { return el.data });
+					break;
+				}
+			}
+		}
+		calculateBallparkMatches();
+	});
+
+
+	function calculateBallparkMatches() {
+		if(typeof $scope.user !== 'undefined' && typeof $scope.friends !== 'undefined') {
+			// map user ballparks to array of ids
+			var userBallparkIds = $scope.userBallparks.map(function(el) {
+				return el.data._id
+			})
+
+			// calculate ballpark matches for user and friends
+			for (var i = 0; i < $scope.friends.length; i++) {
+				var matches = 0;
+				if(typeof $scope.friends[i].ballparks !== 'undefined') {
+					for (var j = 0; j < $scope.friends[i].ballparks.length; j++) {
+						var ballparkId = $scope.friends[i].ballparks[j];
+						if(userBallparkIds.indexOf(ballparkId) !== -1) {
+							// we found a match
+							matches++;
+						}
+					}
+					
+				}
+				$scope.friends[i].matches = matches;
+			}
 		}
 	}
+	
+
+	// $scope.showSearch = function() {
+	// 	if(typeof ballparks !== 'undefined') {
+	// 		$scope.searchMode = true;
+	// 	}
+	// }
 
 	// output search results
 	// $scope.searchMode = true;
-	$scope.showSearchResults = function(event, searchInput) {
-		if(searchInput.length > 1) {
-			$scope.searchMatches = ballparks.filter(function(el) {
-				return el.primary_name.toLowerCase().indexOf(searchInput) !== -1
-						|| el.home_team.toLowerCase().indexOf(searchInput) !== -1;
-				// TODO: exclude stadiums already visited
-			});
-		}
-		else {
-			$scope.searchMatches = null
+	// $scope.showSearchResults = function(event, searchInput) {
+	// 	if(searchInput.length > 1) {
+	// 		$scope.searchMatches = ballparks.filter(function(el) {
+	// 			return el.primary_name.toLowerCase().indexOf(searchInput) !== -1
+	// 					|| el.home_team.toLowerCase().indexOf(searchInput) !== -1;
+	// 			// TODO: exclude stadiums already visited
+	// 		});
+	// 	}
+	// 	else {
+	// 		$scope.searchMatches = null
+	// 	}
+	// }
+
+	$scope.searchBallpark = function(ballparkName) {
+		for(var i = 0; i < ballparks.length; i++) {
+			if(ballparks[i].primary_name.toLowerCase().indexOf(ballparkName.toLowerCase()) !== -1) {
+				$scope.selectedBallpark = ballparks[i];
+				break;
+			}
 		}
 	}
 
-	// add a stadium
-	$scope.addBallpark = function(ballparkId) {
-		console.log(ballparkId);
-		// TODO: add date picker to view
-		UserService.addBallpark(AuthService.getUserId(), ballparkId, '2017-01-20')
+	// add a ballpark
+	$scope.addBallpark = function(ballparkId, dateVisited) {
+		if(typeof dateVisited === 'undefined') {
+			dateVisited = new Date();
+		}
+
+		UserService.addBallpark(AuthService.getUserId(), ballparkId, dateVisited)
 		.then(function() {
 			// reload the user after the ballpark is added
-			loadUser();
+			return loadUser();
+		})
+		.then(function() {
+			$scope.selectedBallpark = null;
 		});
 
 	};
