6066661663ef74cb2fb98bb497ea5231f9710ebd
Luke Adair <j.luke.adair@gmail.com>
2017-03-30 16:31:51 -0500

    removed logout from FB; redirect if logged in

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,10 @@
 app.controller('MainController', function($scope, $window, UserService, FacebookService, AuthService, Facebook) {
 
+	// redirect to my-ballparks if logged in
+	if(AuthService.isTokenValid()) {
+		$window.location.href = '/my-ballparks';
+	}
+
 	$scope.login = function() {
 		FacebookService.login()
 		.then(function(response) {
@@ -23,11 +28,6 @@ app.controller('MainController', function($scope, $window, UserService, Facebook
 
 			// redirect to main page
 			$window.location.href = '/my-ballparks';
-		});
-	};
-
-	$scope.logout = function() {
-		Facebook.logout(function(response) {
 		});
 	};
 
