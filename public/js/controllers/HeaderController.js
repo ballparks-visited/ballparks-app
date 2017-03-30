399724e693b41bb50fa7c17990b13f1229a71926
Luke Adair <j.luke.adair@gmail.com>
2017-03-30 16:29:39 -0500

    hide/show login and logout

--- public/js/controllers/HeaderController.js
+++ public/js/controllers/HeaderController.js
@@ -1,7 +1,16 @@
-app.controller('HeaderController', function($scope, $location) {
+app.controller('HeaderController', function($scope, $location, $window, AuthService) {
 
 	$scope.isActive = function (viewLocation) { 
 		return viewLocation === $location.path();
 	};
+
+	$scope.isLoggedIn = function() {
+		return AuthService.isTokenValid();
+	}
+
+	$scope.logout = function() {
+		AuthService.clearToken();
+		$window.location.href = '/';
+	}
 
 });
