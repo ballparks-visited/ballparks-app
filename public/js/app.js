65764daa3dff31bd60ed11abbd1927468730b7b7
Luke Adair <j.luke.adair@gmail.com>
2017-03-30 16:30:12 -0500

    added directive to watch for enter key

--- public/js/app.js
+++ public/js/app.js
@@ -35,4 +35,17 @@ var app = angular.module('ballparksApp', [
 			});
 		}
 	};
+})
+.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
 });
