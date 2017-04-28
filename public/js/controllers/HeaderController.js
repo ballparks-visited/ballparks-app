c1a96ceb0d0e5a8d651ff22aee2a0568e7cd1062
Jay Gossett <jay.d.gossett+github@gmail.com>
2017-04-28 16:09:55 -0500

    Updated screenshot and login functionality

--- public/js/controllers/HeaderController.js
+++ public/js/controllers/HeaderController.js
@@ -1,4 +1,4 @@
-app.controller('HeaderController', function($scope, $location, $window, AuthService) {
+app.controller('HeaderController', function($rootScope, $scope, $location, $window, AuthService, FacebookService, UserService) {
 
 	$scope.isActive = function (viewLocation) { 
 		return viewLocation === $location.path();
@@ -6,11 +6,36 @@ app.controller('HeaderController', function($scope, $location, $window, AuthServ
 
 	$scope.isLoggedIn = function() {
 		return AuthService.isTokenValid();
-	}
+	};
 
 	$scope.logout = function() {
 		AuthService.clearToken();
 		$window.location.href = '/';
-	}
+	};
+	
+	$scope.login = function () {
+		var loc = $location.path();
+		
+		if (loc == '/') {
+			FacebookService.login()
+			.then(function(response) {
+				loginResponse = response;
+				return FacebookService.getUserData();
+			})
+			.then(function(userResponse) {
+				// save the user
+				var userData = userResponse;
+				angular.extend(userData, loginResponse.authResponse);
+				return UserService.saveUser(userData);
+			})
+			.then(function(jwt) {
+				AuthService.setToken(jwt.data);
+				// redirect to main page
+				$window.location.href = '/my-ballparks';
+			});
+		} else { 
+			$window.location.href = '/' 
+		};
+	};	
 
 });
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,11 +1,10 @@
 app.controller('MainController', function($scope, $window, UserService, FacebookService, AuthService, Facebook) {
-
 	// redirect to my-ballparks if logged in
 	if(AuthService.isTokenValid()) {
 		$window.location.href = '/my-ballparks';
-	}
+	};
 
-	$scope.login = function() {
+	$scope.login = function () {
 		FacebookService.login()
 		.then(function(response) {
 			loginResponse = response;
@@ -19,15 +18,12 @@ app.controller('MainController', function($scope, $window, UserService, Facebook
 			// save the user
 			var userData = userResponse;
 			angular.extend(userData, loginResponse.authResponse);
-
 			return UserService.saveUser(userData);
 		})
 		.then(function(jwt) {
 			AuthService.setToken(jwt.data);
-
 			// redirect to main page
 			$window.location.href = '/my-ballparks';
 		});
-	};
-
+	};	
 });
