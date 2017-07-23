9511795d171f52566a784c931c8ff44770d843c7
Jay Gossett <jay.d.gossett+github@gmail.com>
2017-07-22 20:44:34 -0500

    Removing the remaining sensitive info

--- config/config.js
+++ config/config.js
@@ -11,18 +11,11 @@ if(typeof process.env.appMode !== 'undefined' && process.env.appMode === 'produc
 } /*
 else {
 	// app is running in test mode
-	// module.exports = {
-	// 	mongo_url : 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + ':27017/testAPP',
-	// 	appId: "1272641912758292",
-	// 	appSecret: "09be4141b221d59aa4f39619f6d1cd28",
-	// 	jwtSecret: "6b4d826c2cd3e7d2068ee86948da351e06beb076"
-	// }
-	// app is running in test mode
-	module.exports = {
-		mongo_url : 'mongodb://heroku_q4jgklvp:te4ddga4b1ved2nbeferqndnso@ds157439.mlab.com:57439/heroku_q4jgklvp',
-		appId: "1272641912758292",
-		appSecret: "09be4141b221d59aa4f39619f6d1cd28",
-		jwtSecret: "6b4d826c2cd3e7d2068ee86948da351e06beb076"
-	}
+		module.exports = {
+	 	mongo_url : 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + ':27017/testAPP',
+	 	appId: "",
+	 	appSecret: "",
+	 	jwtSecret: ""
+	 }
 } */
 
